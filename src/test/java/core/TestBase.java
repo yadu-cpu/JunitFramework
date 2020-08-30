3a27460622955ae7154cf1d925c7bf6f062da9d7
yadu_cpu <bhopatkar.yadnesh@gmail.com>
2020-08-30 16:50:11 +0530

    Some files have added.

---
+++ src/test/java/core/TestBase.java
@@ -0,0 +1,27 @@
+package core;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+import webdriverfactory.WebDriverFactory;
+
+public class TestBase {
+    protected WebDriver driver; //this should never be static, if made static parallel exec of classes not possible
+    protected final static String base_url = "https://amazon.in";
+
+    @Before
+    public void set_up(){
+        try{
+            driver = WebDriverFactory.getWebDriverForBrowser("chrome");
+        }catch(Exception e){
+            e.printStackTrace();
+            Assert.fail("Incorrect Browser Sent. Check the Stack Trace");
+        }
+    }
+
+    @After
+    public void clean_up(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/webdriverfactory/WebDriverFactory.java
@@ -0,0 +1,27 @@
+package webdriverfactory;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+	private static WebDriver driver=null;
+	public static WebDriver getWebDriverForBrowser(String browser)throws Exception {
+		switch(browser.toLowerCase()) {
+		case "chrome":
+			driver=new ChromeDriver();
+			break;
+		case "firefox":
+			driver=new FirefoxDriver();
+			break;
+			default:
+				throw new Exception("No such browser is implemented.Browser name sent: " + browser);
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		return driver;
+	}
+}
